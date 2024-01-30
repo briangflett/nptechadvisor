2174b7cf0af93e3506f92161f6f698ba10e24ea3
Brian Flett <brian.g.flett@gmail.com>
2024-01-30 16:35:22 -0500

    Added first blog entry

--- app/blog-index/single-blog.tsx
+++ app/blog-index/single-blog.tsx
@@ -17,12 +17,12 @@ const SingleBlog = ({ blog }: { blog: Blog }) => {
           <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white">
             {tags[0]}
           </span>
-          <Image src={image} alt="image" fill />
+          <Image src={image} alt="image" fill sizes="33vw" />
         </Link>
         <div className="p-6 sm:p-8 md:px-6 md:py-8 lg:p-8 xl:px-5 xl:py-8 2xl:p-8">
           <h3>
             <Link
-              href="/blog-details"
+              href={`/blog/${slug}`}
               className="mb-4 block text-xl font-bold text-black hover:text-primary dark:text-white dark:hover:text-primary sm:text-2xl"
             >
               {title}
@@ -35,7 +35,12 @@ const SingleBlog = ({ blog }: { blog: Blog }) => {
             <div className="mr-5 flex items-center border-r border-body-color border-opacity-10 pr-5 dark:border-white dark:border-opacity-10 xl:mr-3 xl:pr-3 2xl:mr-5 2xl:pr-5">
               <div className="mr-4">
                 <div className="relative h-10 w-10 overflow-hidden rounded-full">
-                  <Image src={author.image} alt="author" fill />
+                  <Image
+                    src={author.image}
+                    alt="author"
+                    fill
+                    sizes="(max-width: 40px)"
+                  />
                 </div>
               </div>
               <div className="w-full">
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -9,7 +9,37 @@ import type { MDXComponents } from "mdx/types";
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    h1: ({ children }) => (
+      <h1 className="mb-5 mt-20 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+        {children}
+      </h1>
+    ),
+
+    p: ({ children }) => (
+      <p className="mb-5 text-base font-medium leading-relaxed text-body-color">
+        {children}
+      </p>
+    ),
+
+    ul: ({ children }) => (
+      <ul
+        className="mb-5 ml-10 block list-disc pl-10 text-base font-medium leading-relaxed text-body-color"
+        style={{
+          display: "block",
+          listStyle: "disc outside none",
+          margin: "1em 0",
+          padding: "0 0 0 40px",
+        }}
+      >
+        {children}
+      </ul>
+    ),
+
+    li: ({ children }) => (
+      <li className="list-outside list-disc pr-[10px] text-base font-medium leading-relaxed text-body-color">
+        {children}
+      </li>
+    ),
     // img: (props) => (
     //   <Image
     //     sizes="100vw"
